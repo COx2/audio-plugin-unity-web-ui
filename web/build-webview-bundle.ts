dd8c9fc47578c6ae26c5a1d26f3f9b1a764a7d92
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2024-12-09 12:04:01 +0900

    Fix bundler script.

--- web/build-webview-bundle.ts
+++ web/build-webview-bundle.ts
@@ -6,24 +6,31 @@ Please refer to the official website for instructions on how to install Deno.
 https://deno.com/
 */
 
-import {$, CommandBuilder} from "https://deno.land/x/dax/mod.ts";
+import { $ } from "https://deno.land/x/dax/mod.ts";
 import { fromFileUrl } from "https://deno.land/std/path/mod.ts";
+import * as fs from "jsr:@std/fs";
+
 
 //============================================
 console.log("Deno.cwd(): " + Deno.cwd());
 
 const __file_directory = fromFileUrl(new URL("./", import.meta.url));
-const __web_directory = fromFileUrl(new URL("../../", import.meta.url));
-const __root_directory = fromFileUrl(new URL("../../../", import.meta.url));
+const __root_directory = fromFileUrl(new URL("../", import.meta.url));
 Deno.chdir(__file_directory);
 
 console.log("Root directory: " + __root_directory);
 console.log("Deno.cwd(): " + Deno.cwd());
 
-await Deno.copyFile("./binding/juce-framework-frontend", "./dist/juce-framework-frontend");
-await Deno.copyFile("./binding/page/*", "./dist/*");
-await Deno.copyFile("./binding/page/juce-binding-impl.js", "./dist/juce-binding-impl.js");
-await Deno.copyFile("./binding/page/index.html", "./dist/index.html");
+await fs.copy("./binding/unity_web_build/Build", "./dist/Build", { overwrite: true });
+await fs.copy("./binding/unity_web_build/TemplateData", "./dist/TemplateData", { overwrite: true });
+await fs.copy("./binding/unity_web_build/manifest.webmanifest", "./dist/manifest.webmanifest", { overwrite: true });
+await fs.copy("./binding/unity_web_build/ServiceWorker.js", "./dist/ServiceWorker.js", { overwrite: true });
+
+await fs.copy("./binding/juce-framework-frontend", "./dist/juce-framework-frontend", { overwrite: true });
+
+await fs.copy("./binding/page/unity-binding-lib.js", "./dist/unity-binding-lib.js", { overwrite: true });
+await fs.copy("./binding/page/juce-binding-impl.js", "./dist/juce-binding-impl.js", { overwrite: true });
+await fs.copy("./binding/page/index.html", "./dist/index.html", { overwrite: true });
 
 Deno.chdir(__file_directory + "dist");
 
